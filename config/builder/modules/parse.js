f3f828e83b86d5aedfa70672e550c228df20edd2
Ken Vu <ken@gladeye.co.nz>
2017-08-31 15:27:39 +1200

    Switch over to use `@kenvunz/shoelace`

--- config/builder/modules/parse.js
+++ config/builder/modules/parse.js
@@ -1,33 +1,6 @@
-const objectPath = require("object-path");
+const objectPath = require("object-path"),
+    { parse } = require("@kenvunz/shoelace");
 
 module.exports = function(options) {
-    let count = 1;
-
-    const fn = function(options) {
-        let json = JSON.stringify(options);
-        const pre = objectPath(options),
-            matches = json.match(/"@{([\w\.]+)}["\/]/g);
-        if (matches) {
-            // replace any "@key" occurrences with the real value
-            matches.forEach(match => {
-                const key = match.replace(/[@{}\/"]/g, ""),
-                    value = pre.get(key);
-                json = json.replace(
-                    new RegExp(match, "g"),
-                    match.substr(-1) === "/"
-                        ? `"${value}/`
-                        : JSON.stringify(value)
-                );
-            });
-        }
-        return JSON.parse(json);
-    };
-
-    // go through 2 passes, make sure we are not missing anything
-    while (count <= 2) {
-        options = fn(options);
-        count += 1;
-    }
-
-    return objectPath(options);
+    return objectPath(parse(options));
 };
