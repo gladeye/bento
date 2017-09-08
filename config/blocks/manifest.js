20522788afb4b621678e5df48249fe87e8d07516
Ken Vu <ken@gladeye.co.nz>
2017-09-08 21:31:05 +1200

    Reset folder base

--- config/blocks/manifest.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(config, options) {
-    options.set("manifest", {});
-};
--- config/builder/modules/parse.js
+++
@@ -1,6 +0,0 @@
-const objectPath = require("object-path"),
-    { parse } = require("@kenvunz/shoelace");
-
-module.exports = function(options) {
-    return objectPath(parse(options));
-};
