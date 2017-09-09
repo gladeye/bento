23437d110890d8ee4f870556b6e7fc290c0ba234
Ken Vu <ken@gladeye.co.nz>
2017-09-10 08:53:40 +1200

    Add `blocks/plugins`

---
+++ __tests__/blocks/plugins.test.js
@@ -0,0 +1,28 @@
+import plugins from "~/blocks/plugins";
+import ConfigBuilder from "~/lib/ConfigBuilder";
+
+describe("blocks/plugins", () => {
+    const options = {
+        filename: "[name]",
+
+        paths: {
+            root: process.cwd(),
+            input: "./app",
+            output: "./public"
+        },
+        files: {
+            copy: "+(images|media)/**/*"
+        },
+        blocks: {
+            list: [plugins]
+        }
+    };
+
+    it("matches snapshot", () => {
+        const builder = ConfigBuilder.create(Object.assign({}, options));
+
+        return builder.build().then(config => {
+            expect(config).toMatchSnapshot();
+        });
+    });
+});
---
+++ config/src/blocks/plugins.js
@@ -0,0 +1,43 @@
+import path from "path";
+import webpack from "webpack";
+import merge from "webpack-merge";
+import CleanPlugin from "clean-webpack-plugin";
+import CopyGlobsPlugin from "copy-globs-webpack-plugin";
+import WebpackAssetsManifest from "webpack-assets-manifest";
+
+export default function(config, options, { manifest }) {
+    return merge(config, {
+        plugins: [
+            new CleanPlugin(
+                [`${path.join(options.get("paths.output"), "**/*")}`],
+                {
+                    root: options.get("paths.root"),
+                    verbose: false
+                }
+            ),
+
+            new CopyGlobsPlugin({
+                pattern: options.get("files.copy"),
+                output: `[path]${options.get("filename")}.[ext]`,
+                manifest: manifest.data
+            }),
+
+            new WebpackAssetsManifest({
+                output: "manifest.json",
+                space: 4,
+                writeToDisk: true,
+                sortManifest: true,
+                assets: manifest.data,
+                replacer: manifest.formatter()
+            }),
+
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify(options.get("env.value"))
+                }
+            }),
+
+            new webpack.NamedModulesPlugin()
+        ]
+    });
+}
