275e0986e76c969cb85a4cf369fb7d5700da8c4a
Ken Vu <ken@gladeye.co.nz>
2017-12-06 19:36:33 +1300

    Spring clean 🌱

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# 1.1.0
+
+* Rewrite the core, reducing footprints
+* Fluent and expressive configuration API
+
+# 1.0.0
+
+* Initial release
--- config/blocks/html.js
+++
@@ -1,7 +0,0 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-module.exports = function(config, options) {
-    config.merge({
-        plugins: [new HtmlWebpackPlugin(options.get("html"))]
-    });
-};
