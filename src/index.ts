ef157cc4a1bb2208faa72c68a370f18c1bfb13c0
Ken Vu <ken@gladeye.co.nz>
2018-01-01 22:20:39 +1300

    Add default `bento` function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import Bento from "~/core/Bento";
+import { Configuration } from "webpack";
+import Bento, { BaseConfig } from "~/core/Bento";
+import StandardBento from "~/presets/StandardBento";
+import PlainBento from "~/presets/PlainBento";
 
-export default Bento;
+export default function bento(
+    config: BaseConfig,
+    env?: string
+): Promise<Configuration> {
+    const instance = StandardBento.create(config);
+    return instance.export(env);
+}
+
+export { StandardBento, PlainBento, Bento };
